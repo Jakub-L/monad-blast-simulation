9a0378279aeca13df1e2f98f3ad48c200fb14a59
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2026-09-05 22:53:34 +0200

    task: add type import linting rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,20 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(js.configs.recommended, tseslint.configs.recommended, {
-  ignores: ["dist", "coverage", "node_modules"]
-});
-
+export default tseslint.config(
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    ignores: ['dist/**', 'coverage/**', 'node_modules/**']
+  },
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports'
+        }
+      ]
+    }
+  }
+);
