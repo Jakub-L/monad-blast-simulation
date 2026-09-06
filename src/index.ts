76ff8ef4c383f1d471ba34ac656801e2a88fd8da
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2026-09-06 23:05:46 +0200

    task: add index file

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Character } from '@/utils/character';
+import { Explosion } from '@/utils/explosion';
+import { Barrier } from '@/utils/barrier';
+import { Simulation } from '@/simulation';
+import { DamageType } from '@/types';
+
+export { Character, Explosion, Barrier, Simulation, DamageType };
